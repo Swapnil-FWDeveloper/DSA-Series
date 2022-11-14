ff46eea804197a29bc982f9d8adf5480b9218d1f
Swapnil Shende <116657863+Swapnil-FWDeveloper@users.noreply.github.com>
2022-11-14 12:35:57 +0530

    Add files via upload

---
+++ BasicDSA/sum of series/Readme.md
@@ -0,0 +1,9 @@
+Question link = https://bespoke-belekoy-cf6681.netlify.app
+
+Hint
+
+Give N = 3, X = 2
+
+We need to find the sum of first N=3 terms, having value of X=2
+First 3 terms in Expression = 1 + x + x*x = 1 + 2 + 2*2 = 7
+Answer is 7
---
+++ BasicDSA/sum of series/index.js
@@ -0,0 +1,27 @@
+
+let N=2;
+let X=5;
+// if(N<=0 && X<=0)
+// {
+//     console.log(-1)
+// }else if(N>=0 && X>=0)
+// {
+//     console.log(1+X+X*X)
+// } This code will not work because it will not testcase like
+//  Input
+// 2 5
+// Expected Output
+// 31
+// Output
+// 7
+alert("code is in index.js")
+alert("Please use this code only when you have tried at lest 5 times-20 minutes dryrun of your code")
+sum=0;
+ if(N<=0 && X<=0)
+{
+    sum+=-1
+}else if(N>=0 && X>=0)
+{
+    sum+=(1+X+X*X)
+}
+console.log(sum)
