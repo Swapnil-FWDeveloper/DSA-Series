4703198b9ee00a854edfe2b3e1881ddfed7280eb
Swapnil Shende <116657863+Swapnil-FWDeveloper@users.noreply.github.com>
2022-12-09 01:42:42 +0530

    Print Substring

---
+++ Subarray and Substring/Print Substring/index.js
@@ -0,0 +1,11 @@
+
+let str='Racecar'
+for(let i=0;i<str.length;i++){
+    let bag=''
+    for(let j=i;j<str.length;j++){
+        bag+=str[j]
+    }
+    console.log(bag)
+}
+
+// You can use function also to print the same output
