e8f3d55199246b2a5ec73fd378bde5bb07c6e176
Swapnil Shende <116657863+Swapnil-FWDeveloper@users.noreply.github.com>
2022-12-14 23:20:47 +0530

    New Data

---
+++ 2D-Array/Print Left Diagonal/index.js
@@ -0,0 +1,19 @@
+
+let mat=[
+        ['a','b','c','d'],
+        ['e','f','g','h'],
+        ['i','j','k','l'],
+        ['m','n','o','p']
+]
+
+for(let i=0;i<mat.length;i++){
+  let bag=''
+  for(let j=0;j<mat.length;j++){
+    if(i==j){
+      bag+=mat[i][j]+" "
+    }else{
+      bag+=" "
+    }
+  }
+  console.log(bag)
+}
